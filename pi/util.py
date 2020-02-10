fcf2e570773d4f2bc0130fe067d886bb32cc6d4d
Ben <reidben24@gmail.com>
2020-02-09 19:45:35 -0500

    Add start stream function to pi python

--- pi/util.py
+++ pi/util.py
@@ -1,5 +1,10 @@
 from urllib import request
+import subprocess
 
 
 def get_ip():
     return request.urlopen('https://api.ipify.org').read().decode('utf8')
+
+
+def start_stream():
+    subprocess.Popen(['./startstream.sh'], shell=True, executable='/bin/bash')
