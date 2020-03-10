6e697f7c6dee56ab380551e05fcde0d1de6a8fc1
Ben <reidben24@gmail.com>
2020-03-09 20:17:37 -0400

    make copy of log on start

--- pi/startup.sh
+++ pi/startup.sh
@@ -5,6 +5,7 @@ sudo systemctl stop serial-getty@ttyAMA0.service
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
+cp $DIR/oatmeal.log $DIR/otameal.log.old
 /usr/bin/python3 -u $DIR/main.py 10 &> $DIR/oatmeal.log &
 echo "Started Oatmeal"
 
