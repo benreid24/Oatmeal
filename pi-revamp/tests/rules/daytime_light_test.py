31ec0df9b20b42e146d955e697f1d3ac827e77b3
Ben Reid <reidben24@gmail.com>
2021-05-29 22:21:28 -0400

    Comprehensively test daytime light rule

--- pi-revamp/tests/rules/daytime_light_test.py
+++ pi-revamp/tests/rules/daytime_light_test.py
@@ -7,8 +7,6 @@ from arduino.command import CommandType
 from arduino.command_set import CommandSet
 from web.message import MessageLevel
 
-EVENING_LIGHT = 'daytime-evening-light'
-
 
 class DaytimeLightTest(unittest.TestCase):
     def test_morning_off(self):
@@ -27,3 +25,59 @@ class DaytimeLightTest(unittest.TestCase):
         self.assertEqual(len(messages), 0)
         self.assertEqual(len(commands), 1)
         self.assertEqual(commands[0].command_type, CommandType.LIGHT_OFF)
+
+    def test_morning_on(self):
+        rule = DaytimeLightRule()
+        commands = CommandSet()
+        datastore = TestDatastore()
+
+        messages = rule.apply(
+            datetime(2021, 5, 16, 9, 24, 0),
+            None,
+            datastore,
+            commands
+        )
+        commands = commands.get_commands()
+
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(len(commands), 1)
+        self.assertEqual(messages[0].level, MessageLevel.INFO)
+        self.assertEqual(commands[0].command_type, CommandType.LIGHT_ON)
+
+        # No duplicate messages
+        messages = rule.apply(
+            datetime(2021, 5, 16, 14, 24, 0),
+            None,
+            datastore,
+            CommandSet()
+        )
+
+        self.assertEqual(len(messages), 0)
+
+    def test_evening_off(self):
+        rule = DaytimeLightRule()
+        commands = CommandSet()
+        datastore = TestDatastore()
+
+        messages = rule.apply(
+            datetime(2021, 5, 16, 21, 0, 0),
+            None,
+            datastore,
+            commands
+        )
+        commands = commands.get_commands()
+
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(len(commands), 1)
+        self.assertEqual(messages[0].level, MessageLevel.INFO)
+        self.assertEqual(commands[0].command_type, CommandType.LIGHT_OFF)
+
+        # No duplicate messages
+        messages = rule.apply(
+            datetime(2021, 5, 16, 23, 24, 0),
+            None,
+            datastore,
+            CommandSet()
+        )
+
+        self.assertEqual(len(messages), 0)
